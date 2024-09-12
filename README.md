8ec0dda9e6ae112dba445ce285fcd4a2e7812403
Liam Wilson <lwilso26@uoguelph.ca>
2024-09-12 11:05:40 -0400

    Getting Data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReproducibilityGitProject
 For LDP
+
+
--- REscripts/datascript.R
+++ REscripts/datascript.R
@@ -1,9 +1,15 @@
 #Aquiring Data
-
 #Getting dataset 
 install.packages("lterdatasampler")
 library(lterdatasampler)
 #View dataset
 hbr_maples
 #Downloading dataset 
-write.csv(hbr_maples)
+write.csv(hbr_maples, "/Users/liamwilson/Desktop/GradSchool/Semesters/FourthSemF2024
+          /ReproducibilityR/ReproducibilityProjectF2024/ReproducibilityGitProject/
+          RErawdata", row.names = FALSE
+
+#Import CSV to global enviroment 
+library(readr)
+hbr_maples <- read_csv("RErawdata/hbr_maples.csv")
+View(hbr_maples)
