b08e0ee76d476816d6b3a6ad63fd52c0dc1422b9
Liam Wilson <lwilso26@uoguelph.ca>
2024-09-27 10:55:20 -0400

    Script Updates

---
+++ 05_REpreregistration/README.R
@@ -0,0 +1,12 @@
+#To initiate new Pre-Registration Rmd
+
+## Download Packages 
+install.packages("prereg")
+install.packages('tinytex')
+tinytex::install_tinytex(force=TRUE) # #If install.packages("tinytex") does not work)
+library(tinytex)
+library(prereg)
+
+
+#After downloading packages, when creating a new Rmd file, select from template and choose
+#one provided by the prereg package. 
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Title: ReproducibilityGitProject
 Author: LW
+
 Description: This project is to be submitted as the final assignment for the LDP reproducibility class.
 
-To reproduce this work, download files through the git repository. 
+ 
 
 
 
